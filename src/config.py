4145d6b0a4fbeedb3b972417a70c23bf967012a3
Sergey <gavrin_s@mail.ru>
2020-03-17 20:16:25 +0700

    first commit

---
+++ src/config.py
@@ -0,0 +1,7 @@
+"""File with configs"""
+import os
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_PATH = os.path.join(ROOT_PATH, "models")
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+LOG_PATH = os.path.join(ROOT_PATH, "logs")
