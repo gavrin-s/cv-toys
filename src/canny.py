b18679660b132558587d28d8767194a6bd2df852
Sergey <gavrin_s@mail.ru>
2020-03-19 22:13:23 +0700

    add canny edge detector

---
+++ src/canny.py
@@ -0,0 +1,49 @@
+"""
+Plays with Canny  Edge Detector
+See tutorial https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_canny/py_canny.html
+"""
+from typing import Any
+import os
+import uuid
+import cv2
+import numpy as np
+
+from src.config import DATA_PATH, TMP_PATH
+
+
+def nothing(x: Any):
+    pass
+
+
+def main(image_source: np.ndarray):
+    cv2.namedWindow('Image')
+
+    # create trackbars for color change
+    cv2.createTrackbar('Threshold 1', 'Image', 0, 1000, nothing)
+    cv2.createTrackbar('Threshold 2', 'Image', 0, 1000, nothing)
+    cv2.createTrackbar('L2gradient', 'Image', 0, 1, nothing)
+
+    image_egdes = cv2.Canny(image_source, 100, 200, L2gradient=True)
+
+    while True:
+        cv2.imshow('Image', image_egdes)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27 or k == 113:
+            break
+        elif k == 115:
+            cv2.imwrite(os.path.join(TMP_PATH, f"{uuid.uuid4().hex}.jpg"), image_egdes)
+
+        threshold1 = cv2.getTrackbarPos('Threshold 1', 'Image')
+        threshold2 = cv2.getTrackbarPos('Threshold 2', 'Image')
+        l2gradient = cv2.getTrackbarPos('L2gradient', 'Image')
+
+        image_egdes = cv2.Canny(image_source, threshold1=threshold1, threshold2=threshold2,
+                                L2gradient=l2gradient)
+
+    cv2.destroyAllWindows()
+
+
+# Use `ESC` or `q` for quit, `s` for save image in tmp folder
+if __name__ == "__main__":
+    image = cv2.imread(os.path.join(DATA_PATH, "spiderman.png"))
+    main(image)
--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ import os
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(ROOT_PATH, "models")
 DATA_PATH = os.path.join(ROOT_PATH, "data")
+TMP_PATH = os.path.join(ROOT_PATH, "tmp")
 LOG_PATH = os.path.join(ROOT_PATH, "logs")
